303ed2d6ea0bdf65a7f6cdce0de8e5520e6e4824
Matt Boetger <matt.boetger@gmail.com>
2018-03-19 00:53:32 -0400

    build script

---
+++ scripts/build-wasm.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+emcc src/math/backends/wasm/matmul.cpp -s WASM=1 -s ASSERTIONS=1 -g2 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall','cwrap']" -s EXPORTED_FUNCTIONS="['_matmul']" -o demos/benchmarks/matmul.js
+cp src/math/backends/wasm/matmul.wasm demos/benchmarks/matmul.wasm
+scripts/build-npm.sh
+rm -rf demos/node_modules/deeplearn/* && tar -zxf deeplearn-0.4.1.tgz -C /tmp/ && mv -f /tmp/package/dist/* demos/node_modules/deeplearn/
+cp src/math/backends/wasm/wasm-arrays.d.ts demos/node_modules/deeplearn/math/backends/wasm/
+cp src/math/backends/wasm/webassembly.d.ts demos/node_modules/deeplearn/math/backends/wasm/
